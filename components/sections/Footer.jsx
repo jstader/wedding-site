489ae0c8ea0949d10e6316764ebcde67f97364c1
John Stader <47798010+jstader@users.noreply.github.com>
2023-06-07 21:08:35 -0700

    Update Footer.jsx
    Modified footer

--- components/sections/Footer.jsx
+++ components/sections/Footer.jsx
@@ -7,18 +7,7 @@ function Footer() {
       <PageSection>
         <InnerWrapper>
           <p className='max-w-2xl'>
-            This site was built by hand by Jordan, who fundamentally believes in open and accessible
-            education. As such, you&apos;re welcome to reuse, steal, and learn from the code{' '}
-            <Link href='https://github.com/jordanlambrecht/ope-we-eloped' passHref>
-              <a
-                target={'_blank'}
-                rel=' noreferrer'
-                className='font-semibold border-b hover:opacity-80 duration-300'
-              >
-                here
-              </a>
-            </Link>
-            .
+            This site was built with love by John with input from Alexis, Nila, and Suzy.
           </p>
         </InnerWrapper>
       </PageSection>
