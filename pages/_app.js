919a03371c74b37739646b16f2612bf13f998fec
John Stader <47798010+jstader@users.noreply.github.com>
2023-06-07 21:27:02 -0700

    Update _app.js
    Change author name

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,13 @@ import Head from 'next/head'
 import Navigation from 'components/Navigation'
 function MyApp({ Component, pageProps }) {
   console.log(
-    'Created by Jordan Lambrecht and Bergen Johnston in Lincoln, Nebraska. This code was open-sourced in 2022 under an MIT licenes. The original github repo can be found here: https://github.com/jordanlambrecht/ope-we-eloped',
+    'Created by John Stader. This code is based on a website developed by Jordan Lambrecht and released under MIT license here: https://github.com/jordanlambrecht/ope-we-eloped',
   )
-  console.log('#BlackLivesMatter #ACAB')
   return (
     <>
       <Head>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='author' content='Jordan Lambrecht'></meta>
+        <meta name='author' content='John Stader'></meta>
         <link rel='shortcut icon' href='/img/favicon.png' />
       </Head>
       <Script src='https://www.googletagmanager.com/gtag/js?id=G-ZWPNDC86VY' />{' '}
