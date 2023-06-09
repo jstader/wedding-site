2ab958e95b75ada3feb442f635f84e65483b75ec
John Stader <john.stader@afit.edu>
2023-06-09 15:18:00 -0700

    Correct <a> issues from legacy

--- components/parts/Button_Filled.jsx
+++ components/parts/Button_Filled.jsx
@@ -2,19 +2,19 @@ import Link from 'next/link'
 import cn from 'classnames'
 function Button_Filled({ url, text, className }) {
   return (
-    <Link href={url} passHref>
-      <a
-        className={cn(
-          'text-center inline-block mt-4 font-medium rounded-md text-lg px-6 py-2 bg-black text-cream-light drop-shadow-sm duration-300 ease-in-out hover:scale-95',
-          [`${className}`],
-        )}
-        rel='noOpener'
-        target={'_blank'}
-      >
-        {text} &rarr;
-      </a>
-    </Link>
-  )
+    (<Link
+      href={url}
+      passHref
+      className={cn(
+        'text-center inline-block mt-4 font-medium rounded-md text-lg px-6 py-2 bg-black text-cream-light drop-shadow-sm duration-300 ease-in-out hover:scale-95',
+        [`${className}`],
+      )}
+      rel='noOpener'
+      target={'_blank'}>
+
+      {text}→
+    </Link>)
+  );
 }
 
 export default Button_Filled
--- components/parts/Spotify_Playlist.jsx
+++ components/parts/Spotify_Playlist.jsx
@@ -15,25 +15,29 @@ export default function Spotify_Playlist() {
     <div className='border-2 border-black rounded-md py-4 flex flex-col px-4'>
       <div className='flex flex-col md:flex-row gap-4 px-3 md:gap-12 md:my-4 md:pt-4'>
         <div className='flex-row'>
-          <Link href={data.external_urls.spotify} passHref>
-            <a className={'cursor-pointer'} target='_blank'>
-              <div className='relative rounded-md overflow-hidden md:mx-auto w-32 h-32 md:w-64 md:h-64'>
-                <Image
-                  src={data.albumCoverImage}
-                  layout='fill'
-                  objectFit='cover'
-                  placeholder='blur'
-                  blurDataURL={data.albumCoverImage}
-                  quality={25}
-                  className='w-full h-full absolute'
-                  alt={data.album}
-                />
-              </div>
-            </a>
+          <Link
+            href={data.external_urls.spotify}
+            passHref
+            className={'cursor-pointer'}
+            target='_blank'>
+
+            <div className='relative rounded-md overflow-hidden md:mx-auto w-32 h-32 md:w-64 md:h-64'>
+              <Image
+                src={data.albumCoverImage}
+                layout='fill'
+                objectFit='cover'
+                placeholder='blur'
+                blurDataURL={data.albumCoverImage}
+                quality={25}
+                className='w-full h-full absolute'
+                alt={data.album}
+              />
+            </div>
+
           </Link>
         </div>
         <div className='justify-end '>
-          <Link href={data.external_urls.spotify} passHref>
+          <Link href={data.external_urls.spotify} passHref legacyBehavior>
             <h2
               color='black'
               className='tracking-wide lg:tracking-wider align-top s font-semibold max-w-md mt-0 text-2xl sm:text-5xl md:text-6xl 2xl:text-6xl 2xl:max-w-3xl leading-none mb-2 '
@@ -64,28 +68,28 @@ export default function Spotify_Playlist() {
           <ul className='grid gap-3 md:gap-2'>
             {data.tracksSelected.map((track, index) => (
               <li key={index} className='my-0'>
-                <Link href={track.songUrl}>
-                  <a target={'_blank'}>
-                    <div className='flex gap-4 md:gap-x-4'>
-                      <Image
-                        quality={50}
-                        layout='fixed'
-                        height={32}
-                        width={32}
-                        src={track.albumArt}
-                        alt='spotify album cover'
-                      />
+                <Link href={track.songUrl} target={'_blank'}>
 
-                      <div className='flex flex-col justify-center'>
-                        <p className='pb-1 text-wine mt-0 mb-0 py-0 leading-none text-sm md:text-md line-clamp-2 '>
-                          {track.title}
-                        </p>
-                        <span className=' my-0 pt-0 mb-0 pb-0 leading-tight text-wine text-opacity-75 text-xs line-clamp-1'>
-                          {track.artist} – {track.album}
-                        </span>
-                      </div>
+                  <div className='flex gap-4 md:gap-x-4'>
+                    <Image
+                      quality={50}
+                      layout='fixed'
+                      height={32}
+                      width={32}
+                      src={track.albumArt}
+                      alt='spotify album cover'
+                    />
+
+                    <div className='flex flex-col justify-center'>
+                      <p className='pb-1 text-wine mt-0 mb-0 py-0 leading-none text-sm md:text-md line-clamp-2 '>
+                        {track.title}
+                      </p>
+                      <span className=' my-0 pt-0 mb-0 pb-0 leading-tight text-wine text-opacity-75 text-xs line-clamp-1'>
+                        {track.artist} – {track.album}
+                      </span>
                     </div>
-                  </a>
+                  </div>
+
                 </Link>
               </li>
             ))}
@@ -93,5 +97,5 @@ export default function Spotify_Playlist() {
         </div>
       </div>
     </div>
-  )
+  );
 }
